4cf14587d5fc95b8ed3a0ba3b2ffeaab88e332d0
doyougnu <jeffrey.young@iohk.io>
2022-12-11 13:43:20 -0500

    trying out packages

---
+++ 5.el
@@ -0,0 +1,27 @@
+;;; 5.el --- problem 5 -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2022 Jeff Young
+;;
+;; Author: Jeff Young <jeff@doyougnu.xyz>
+;; Maintainer: Jeff Young <jeff@doyougnu.xyz>
+;; Created: December 11, 2022
+;; Modified: December 11, 2022
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
+;; Homepage: https://github.com/doyougnu/5
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  problem 5
+;;
+;;; Code:
+
+(require 'utils)
+
+
+
+(provide '5)
+;;; 5.el ends here
---
+++ aoc-pkg.el
@@ -0,0 +1,25 @@
+;;; aoc-pkg.el --- aoc package -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2022 Jeff Young
+;;
+;; Author: Jeff Young <jeff@doyougnu.xyz>
+;; Maintainer: Jeff Young <jeff@doyougnu.xyz>
+;; Created: December 11, 2022
+;; Modified: December 11, 2022
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
+;; Homepage: https://github.com/doyougnu/aoc-pkg
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  aoc package
+;;
+;;; Code:
+
+(define-package "aoc" "0.0.1" "" '((dash "2.19.1")))
+
+(provide 'aoc-pkg)
+;;; aoc-pkg.el ends here
